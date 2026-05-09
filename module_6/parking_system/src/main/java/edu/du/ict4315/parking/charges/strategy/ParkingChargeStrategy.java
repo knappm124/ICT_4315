fe4800792ec28893db2a05bcad0a95dfd63b2355
knappm124 <knappm124@gmail.com>
2026-05-08 20:24:23 -0400

    created module 6 folder

---
+++ module_6/parking_system/src/main/java/edu/du/ict4315/parking/charges/strategy/ParkingChargeStrategy.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package edu.du.ict4315.parking.charges.strategy;
+
+import edu.du.ict4315.currency.Money;
+import edu.du.ict4315.parking.ParkingLot;
+import edu.du.ict4315.parking.ParkingPermit;
+import java.util.HashMap;
+
+/**
+ *
+ * @author melyg
+ */
+public interface ParkingChargeStrategy {
+    public Money parkingCharge(ParkingLot lot, HashMap<String,Boolean> days, Integer timeParked, ParkingPermit permit);
+    public String getStrategyName();
+}
